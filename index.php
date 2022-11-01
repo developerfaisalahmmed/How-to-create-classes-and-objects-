1f41abd51ec3fb0602a65d8c57768ded0e23fa15
Faisal Ahmmed <86137950+developerfaisalahmmed@users.noreply.github.com>
2022-11-01 19:16:58 +0600

    How to create classes and objects?

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// How to create classes and objects?
+
+// Car is a class
+class Car{
+    // properties
+    public $comp;
+    public $color = "beige";
+    public $hasSunRoof = true;
+
+    //method / function
+        public function hello(){
+            return "beep";
+        }
+
+
+}
+
+$bmw = new Car;
+
+// echo $bmw->color;
+// echo $bmw->color = 'blue';
+echo $bmw->hello();
+
+?>
